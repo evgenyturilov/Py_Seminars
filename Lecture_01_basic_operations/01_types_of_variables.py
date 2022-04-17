bd43a5be62efdab9c59ecbcf76f9dadc8774a084
Evgeny Turilov <evgenyturilov@gmail.com>
2022-04-17 17:40:58 +0300

    rename faile, add code

---
+++ Lecture_01_basic_operations/01_types_of_variables.py
@@ -0,0 +1,22 @@
+# типы данных и переменная
+# int, float, boolean, str, list, None
+
+a = 123
+b = 1.23
+print(type(a))
+print(type(b))
+
+value = None        # присваевает переменной тип None, после этого можно использовать неназванную пепеменную
+                    # т.е. значение можно будет присвоить впоследствии
+print(type(value))
+
+value = 1234
+print(type(value))
+
+# работа со строковыми данными
+
+s = 'hello world'
+t = "hello 'world'" # можно t = 'hello "world"'
+u = 'hello \nworld' # \n - оператор переноса на новую строку
+
+print(s, t, u)
