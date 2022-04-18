ad955ba516a0c72d158f426a00baa994885180c2
evgenyturilov <67696898+evgenyturilov@users.noreply.github.com>
2022-04-18 11:27:41 +0300

    Update 04_Task_4.py
    solved Task_4

--- 01_Seminar_1&2_tasks_1-10/04_Task_4.py
+++ 01_Seminar_1&2_tasks_1-10/04_Task_4.py
@@ -1,4 +1,15 @@
 # Задача 4. Показать первую цифру дробной части числа.
 
-number = 3.1415926535
-print(number/10)
+number = input('Введите вещественное число: ')
+print(number)
+# print(type(number))
+# print(len(number))
+
+def find_digit(n):
+    for i in range(len(number) - 1):
+        if number[i] == '.' or ',':
+            result_digit = number[i + 1]
+    return result_digit
+        
+print('Первая цифра дробной части числа:')
+print(find_digit(number))
