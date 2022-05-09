bd78c69f93308d61b4ec90cb2c33122795b42862
Evgeny Turilov <evgenyturilov@gmail.com>
2022-05-09 11:21:50 +0300

    add file with function of fibonacci numbers calculation

---
+++ Fibonacci.py
@@ -0,0 +1,8 @@
+def fibonacci(n):
+    fibonacci_first = 1
+    fibonacci_second = 1
+    for i in range(2, n):
+        fibonacci_n = fibonacci_first + fibonacci_second
+        fibonacci_first = fibonacci_second
+        fibonacci_second = fibonacci_n
+    return fibonacci_n
