3bd1716b933e650d845af5627f25f49a458698e9
Evgeny Turilov <evgenyturilov@gmail.com>
2022-05-09 11:24:35 +0300

    added files with code from Seminar 3 tasks

---
+++ 02_Seminar_3&4_tasks_11-31/Seminar_3/02_Task_2.py
@@ -0,0 +1,30 @@
+# Напишите программу, которая определяет позицию второго вхождения строки в списке, либо сообщит, что ее нет.
+# Пример:
+#           ["qwe", "asd", "zxc", "qwe", "ewq"] - ищем "qwe", ответ:    3
+#           ["123", "234", 123, "567", "789"]   - ищем "123", ответ:    -1
+#           []                                  - ищем "123", ответ:    -1
+
+# Решение №1 (моё)
+
+my_list = ["qwe", "wer", "ert", "rty", "qwe", "yui", "qwe"]
+string_to_find = "qwe"
+flag = -1
+count = 0 
+
+for i in range(len(my_list)):
+    if my_list[i] == string_to_find:
+        count += 1
+        if count == 2:
+            flag = i
+            break
+
+print(flag)
+
+# Решение №2
+
+my_list_2 = ["qwe", "wer", "ert", "rty", "qwe", "yui", "qwe", "rty"]
+list_2 = [i for i,x in enumerate(my_list_2) if x == "wer"]
+if len(list_2) > 1: print(list_2[1])
+else: print('-1')
+
+
---
+++ 02_Seminar_3&4_tasks_11-31/Seminar_3/03_Task_3.py
@@ -0,0 +1,11 @@
+# Реализуйте алгоритм задания случайных чисел без использования генератора псевдослучайных чисел
+
+import time
+
+def random_number(min, max):
+    now = str(time.time())
+    print(now)
+    rnd = int(now[::-1][:3:])/1000
+    return min + rnd*(max-min)
+
+print(round(random_number(1,10)))
