eebc3219896583a069e5568b396f70d84a06d1b6
Evgeny Turilov <evgenyturilov@gmail.com>
2022-05-17 12:03:20 +0300

    added 2 additional methods for solving the task

--- 02_Seminar_3&4_tasks_11-31/12_Task_22.py
+++ 02_Seminar_3&4_tasks_11-31/12_Task_22.py
@@ -2,9 +2,18 @@
 
 lst = [1,2,3,4]
 
-sum = 0
-for i in range(len(lst)):
-    if i%2 == 0:
-        sum += lst[i]
+# Решение 1 (алгоритмическое)
 
-print(sum)
+summ = 0
+for i in range(len(lst)):
+    if i%2 != 0:
+     summ += lst[i]
+print(summ)
+
+# Решение 2 (list comprehention)
+
+print(sum([lst[i] for i in range(len(lst)) if i%2]))
+
+# Решение 3 (lambda + filter())
+
+print(sum(list(filter(lambda i: not i%2, lst))))
