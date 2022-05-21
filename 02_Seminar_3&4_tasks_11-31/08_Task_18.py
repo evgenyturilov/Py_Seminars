d25db1796e7e6c5059e1bab1287c55d0a4784165
Evgeny Turilov <evgenyturilov@gmail.com>
2022-05-21 14:18:25 +0300

    added code to Task_18

--- 02_Seminar_3&4_tasks_11-31/08_Task_18.py
+++ 02_Seminar_3&4_tasks_11-31/08_Task_18.py
@@ -1,1 +1,17 @@
 # Задача 18. Реализовать алгоритм перемешивания списка. 
+
+import random
+
+phrase = "Существует два рычага управления людьми: первый — это личная выгода, а второй — это деньги."
+lst = phrase.split()
+
+
+def shuffle(lst):
+    for i in range(10):
+        tmp = lst[i]
+        m = random.randint(1, len(lst))
+        lst[i] = lst[m]
+        lst[m] = tmp
+    return(' '.join(lst))
+
+print(shuffle(lst))
