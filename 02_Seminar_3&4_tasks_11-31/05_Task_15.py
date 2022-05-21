15a4c5cde8a48273b701620ceb59de5843f57a69
Evgeny Turilov <evgenyturilov@gmail.com>
2022-05-21 10:02:33 +0300

    added code to Task_15

--- 02_Seminar_3&4_tasks_11-31/05_Task_15.py
+++ 02_Seminar_3&4_tasks_11-31/05_Task_15.py
@@ -3,17 +3,15 @@
 
 n = 4
 nums = [i for i in range(1, n + 1)]
+nums.insert(0, 0)
 print(nums)
 
 nums_2 = []
-for i in range(len(nums) + 1):
-    if i == 0:
+for i in range(1 ,len(nums)):
+    if i == 1:
         nums_2.append(1)
     else:
         nums_2.append(nums[i-1]*i)
+        nums[i] = nums[i-1]*i
 
 print(nums_2)
-    
-
-
-#print(list(map(lambda x: (x - 1)*x, nums)))
